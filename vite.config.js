efacfcadc95b1cac2df665476bcaeba36c993db4
Martman954 <matej.palas954@gmail.com>
2025-04-25 12:23:37 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/portfolio-website/",
   plugins: [react(),
     tailwindcss(),
   ],
-  base: "/portfolio-website/"
 })
